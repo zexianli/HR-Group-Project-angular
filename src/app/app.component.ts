43e79a6136d1ca7b44cbb65e78fd02ab1223c480
Zexian <zexianlie@gmail.com>
2025-12-24 19:11:43 -0800

    change to v16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'HR-Project';
