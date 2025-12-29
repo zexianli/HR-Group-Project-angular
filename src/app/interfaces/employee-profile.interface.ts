fb9c363f96e5983b774ee497bd1fee8f97f00051
Tarcisius Daniel <tarcisiusdaniel@gmail.com>
2025-12-29 02:14:45 -0800

    HP-77 Create Employee Profiles page

---
+++ src/app/interfaces/employee-profile.interface.ts
@@ -0,0 +1,93 @@
+//
+
+export type WorkAuth =
+  | 'PENDING'
+  | 'CITIZEN'
+  | 'GREEN_CARD'
+  | 'H1B'
+  | 'L2'
+  | 'F1_CPT_OPT'
+  | 'H4'
+  | 'OTHER';
+
+export type Gender = 'MALE' | 'FEMALE' | 'NO_ANSWER';
+
+export interface EmergencyContact {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  phone: string;
+  email: string;
+  relationship: string;
+}
+
+export interface Address {
+  buildingApt: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+}
+
+export interface Car {
+  make: string;
+  model: string;
+  color: string;
+}
+
+export interface Reference {
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  phone: string;
+  email: string;
+  relationship: string;
+}
+
+//
+
+export interface EmployeeSummaryResponse {
+  totalCount: number;
+  employees: EmployeeSummaryProfile[];
+}
+
+export interface EmployeeProfileResponse {
+  employee: EmployeeProfile;
+}
+
+export interface EmployeeSummaryProfile {
+  id: string;
+  userId: string;
+  name: string;
+  ssn: string;
+  workAuthorizationTitle: WorkAuth;
+  phone: string;
+  email: string;
+  username: string;
+}
+
+export interface EmployeeProfile {
+  _id: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  preferredName: string;
+  ssn: string;
+  dateOfBirth: string;
+  gender: Gender;
+  profilePictureKey: string;
+  cellPhone: string;
+  workPhone: string;
+  workAuthorizationType: WorkAuth;
+  otherWorkAuthorizationTitle: string;
+  workAuthorizationStart: string;
+  workAuthorizationEnd: string;
+  workAuthorizationDocKey: string;
+  driverLicenseDocKey: string;
+  emergencyContacts: EmergencyContact[];
+  houseId: string;
+  address: Address;
+  carInformation: Car;
+  reference: Reference;
+}
--- src/app/pages/main/employee-profile/employee-profile.component.ts
+++ src/app/pages/main/employee-profile/employee-profile.component.ts
@@ -1,8 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { take, switchMap } from 'rxjs/operators';
+import {
+  EmployeeSummaryResponse,
+  EmployeeSummaryProfile,
+} from 'src/app/interfaces/employee-profile.interface';
+import { EmployeeProfileService } from 'src/app/service/employee-profile.service';
+import { selectToken } from 'src/app/store/selectors/auth.selectors';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
+
+//  Allows HR to see a summary of each employee’s profile, search for a
+// particular employee, and view their entire profile.
 
 @Component({
   selector: 'app-employee-profile',
+  standalone: true,
   templateUrl: './employee-profile.component.html',
   styleUrls: ['./employee-profile.component.css'],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+  ],
 })
-export class EmployeeProfileComponent {}
+export class EmployeeProfileComponent implements OnInit, AfterViewInit {
+  employeeSummaries: EmployeeSummaryResponse | null = null;
+  dataSource = new MatTableDataSource<EmployeeSummaryProfile>([]);
+  displayedColumns: string[] = [
+    'name',
+    'ssn',
+    'workAuthorizationTitle',
+    'phone',
+    'email',
+  ];
+  isLoading = true;
+  totalCount = -1;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private store: Store,
+    private employeeProfileService: EmployeeProfileService
+  ) {}
+
+  // call the API to get all EmployeeProfiles
+  // token$ = this.store.select(selectToken);
+  ngOnInit() {
+    this.store
+      .select(selectToken)
+      .pipe(
+        take(1),
+        switchMap(token => {
+          if (!token) {
+            throw new Error('Token invalid:: getEmployeeSummary');
+          }
+          return this.employeeProfileService.getEmployeeSummary(token);
+        })
+      )
+      .subscribe({
+        next: data => {
+          this.employeeSummaries = data;
+          this.dataSource.data = data.employees;
+          this.totalCount = data.totalCount;
+          this.isLoading = false;
+          this.dataSource.paginator = this.paginator;
+          console.log('employeeSummaries:', data.employees);
+        },
+        error: err => {
+          this.isLoading = false;
+          console.error('Getting employee summary error: ', err);
+        },
+      });
+  }
+
+  ngAfterViewInit(): void {
+    if (this.dataSource) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  handleElementClick() {
+    console.log('hello world');
+  }
+}
